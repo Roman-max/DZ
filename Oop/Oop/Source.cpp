9dc7d8d72dfe77fd2d4457510ce173af156af535
Roman <rpavlushko@gmail.com>
2019-11-30 11:18:14 +0200

    Oop

---
+++ Oop/Oop/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Room 
+{
+private:
+	double width;
+	double height;
+	char* world;
+
+public:
+
+};
+
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
